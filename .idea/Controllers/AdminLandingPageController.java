3e8da544cc64195321d51ccc70ae86e300496223
Daniel Ratzlaff <Asylumgames39@gmail.com>
2023-01-23 19:22:26 -0600

    changed to .java format

---
+++ .idea/Controllers/AdminLandingPageController.java
@@ -0,0 +1,20 @@
+@Controller
+@RequestMapping("Users")
+public class UserController {
+
+    @Autowired
+    private UserRepository UserRepository;
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "All Users");
+        List Users = (List<User>) UserRepository.findAll();
+        model.addAttribute("Users", Users );
+        return "Users/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        return "Users/add";
+    }
