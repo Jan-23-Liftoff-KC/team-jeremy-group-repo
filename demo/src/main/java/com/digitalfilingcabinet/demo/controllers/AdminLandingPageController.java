06f6b00c629fec17b5fa947543967b91abef7195
FatedMarigold <110205236+FatedMarigold@users.noreply.github.com>
2023-01-26 20:51:01 -0600

    Admin start

--- demo/src/main/java/com/digitalfilingcabinet/demo/controllers/AdminLandingPageController.java
+++ demo/src/main/java/com/digitalfilingcabinet/demo/controllers/AdminLandingPageController.java
@@ -1,8 +1,44 @@
 package com.digitalfilingcabinet.demo.controllers;
 
+import com.digitalfilingcabinet.demo.models.data.UserListRepository;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import com.digitalfilingcabinet.demo.models.*;
+import com.digitalfilingcabinet.demo.models.data.*;
+
+
+@Controller
 public class AdminLandingPageController {
     // handler method that displays the homepage
     // will render the index.html template. or whatever we call it
     // maybe adminLandingPage.html
+
+    @Autowired
+    private CategoryListRepository categoryListRepository;
+
+    @Autowired
+    private UserListRepository userListRepository;
+
+    @Autowired
+    private RecordsRepository recordsRepository;
+
+    @RequestMapping("")
+    public String index(Model model){
+
+        List users = (List<User>) userListRepository.findAll();
+        model.addAttribute("users",users);
+
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String displayAddCategoryForm(){
+
+    }
+
 
 }
