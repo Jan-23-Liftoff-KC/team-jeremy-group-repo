531e3e69b4883108e956437f2ba7c32d0439dacd
dhalamicek <110735781+dhalamicek@users.noreply.github.com>
2023-01-31 01:11:37 -0600

    Added relationship controller, repo, and class so it can be added to the user

---
+++ demo/src/main/java/com/digitalfilingcabinet/demo/controllers/RelationshipController.java
@@ -0,0 +1,5 @@
+package com.digitalfilingcabinet.demo.controllers;
+
+public class RelationshipController {
+    // this thing should render a table showing all the relationships that are created
+}
