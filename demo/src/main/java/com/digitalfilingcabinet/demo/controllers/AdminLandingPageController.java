5f04530b7accda8b488ae65463b59a7f6d5f1f7f
FatedMarigold <110205236+FatedMarigold@users.noreply.github.com>
2023-02-05 18:02:09 -0600

    continuedAdmin

--- demo/src/main/java/com/digitalfilingcabinet/demo/controllers/AdminLandingPageController.java
+++ demo/src/main/java/com/digitalfilingcabinet/demo/controllers/AdminLandingPageController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import com.digitalfilingcabinet.demo.models.*;
 import com.digitalfilingcabinet.demo.models.data.*;
 
+import javax.validation.Valid;
+
 
 @Controller
 public class AdminLandingPageController {
@@ -30,15 +32,72 @@ public class AdminLandingPageController {
     public String index(Model model){
 
         List users = (List<User>) userListRepository.findAll();
-        model.addAttribute("users",users);
+        model.addAttribute("title", "title");
+        model.addAttribute("users", users);
 
         return "index";
     }
 
     @GetMapping("add")
-    public String displayAddCategoryForm(){
+    public String displayAddCategoryForm(Model model){
+        model.addAttribute("title", "title");
+        model.addAttribute(new CategoryList());
 
+        return "add";
     }
+
+    @PostMapping("add")
+    public String processAddCategoryForm(@ModelAttribute @Valid CategoryList newCategory, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "title");
+            return "add";
+        }
+
+        CategoryListRepository.save(newCategory);
+
+        return "redirect:";
+    }
+
+    @GetMapping("addUser")
+    public String displayAddUserForm(Model model){
+        model.addAttribute("title", "title");
+        model.addAttribute(new UserList());
+
+        return "addUser";
+    }
+
+    @PostMapping("addUser")
+    public String processAddCategoryForm(@ModelAttribute @Valid UserList newUser, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "title");
+            return "addUser";
+        }
+
+        UserListRepository.save(newUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("addRecord")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title", "title");
+        model.addAttribute(new RecordsList());
+
+        return "addRecord";
+    }
+
+    @PostMapping("addRecord")
+    public String processAddCategoryForm(@ModelAttribute @Valid RecordsList newRecord, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "title");
+            return "addRecord";
+        }
+
+        UserListRepository.save(newRecord);
+
+        return "redirect:";
+    }
+
 
 
 }
